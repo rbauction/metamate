afa21e16dad96b7b79cdc5506f298db18131ec72
Andrey Shevtsov <ashevtsov@rbauction.com>
2016-07-08 18:35:31 -0700

    Initial commit

---
+++ dependency_finder.py
@@ -0,0 +1,92 @@
+import json
+
+from sfdclib import \
+    SfdcLogger, \
+    SfdcSession, \
+    SfdcToolingApi
+
+
+def find_test_class_dependencies(log, session, test_classes):
+    tooling = SfdcToolingApi(session)
+    log.inf("Retrieving list of Apex classes from Salesforce")
+    r = tooling.anon_query("SELECT Id,Name FROM ApexClass")
+    j = json.loads(r)
+    apex_classes = {}
+    for c in j['records']:
+        apex_classes[c['Name']] = c['Id']
+
+    log.inf("Looking for tests that exist both locally and in Salesforce")
+    matching_tests = {}
+    for c in test_classes:
+        if c in apex_classes:
+            matching_tests[apex_classes[c]] = c
+
+    container = {'Name': 'AndreyMetadataContainerTest'}
+    log.inf("Checking if metadata container still exists in Salesforce")
+    r = tooling.anon_query("SELECT Id,Name FROM MetadataContainer WHERE Name='%s'" % container['Name'])
+    j = json.loads(r)
+    if j['size'] == 1:
+        log.inf("===> Deleting stale metadata container")
+        r = tooling.delete('/sobjects/MetadataContainer/%s' % j['records'][0]['Id'])
+
+    log.inf("===> Creating metadata container")
+    r = tooling.post('/sobjects/MetadataContainer/', {'Name': container['Name']})
+    j = json.loads(r)
+    if j['success']:
+        container['Id'] = j['id']
+    else:
+        raise Exception('Could not create metadata container')
+
+    members = {}
+    log.inf("===> Adding %s ApexClasses to metadata container" % len(matching_tests))
+    for id, name in matching_tests.items():
+        log.inf("  Id: %s Name: %s" % (id, name))
+        log.dbg("Retrieving object from Salesforce")
+        r = tooling.get('/sobjects/ApexClass/%s/' % id)
+        apexclass = json.loads(r)
+
+        log.dbg("Adding ApexClassMember to the metadata container")
+        r = tooling.post('/sobjects/ApexClassMember/', {
+            'MetadataContainerId': container['Id'],
+            'ContentEntityId': id,
+            'Body': apexclass['Body']})
+        j = json.loads(r)
+        members[j['id']] = name
+
+    log.inf("===> Compiling metadata")
+    r = tooling.post('/sobjects/ContainerAsyncRequest/', {
+        'MetadataContainerId': container['Id'],
+        'IsCheckOnly': 'true'})
+    j = json.loads(r)
+    if j['success']:
+        req_id = j['id']
+    while True:
+        r = tooling.anon_query("SELECT State,ErrorMsg FROM ContainerAsyncRequest WHERE Id='%s'" % req_id)
+        j = json.loads(r)
+        log.inf(j['records'][0]['State'])
+        if j['records'][0]['State'] not in ['Queued', 'Pending', 'InProgress']:
+            break
+        time.sleep(5)
+
+    log.inf("===> Retrieving symbol tables")
+    # Format {'class_name': ['test_class_name1', 'test_class_name2'], ...}
+    dependencies = {}
+    for id, name in members.items():
+        log.inf("  Id: %s Name: %s" % (id, name))
+        r = tooling.get('/sobjects/ApexClassMember/%s/' % id)
+        j = json.loads(r)
+        if j['SymbolTable'] == None:
+            continue
+        for c in j['SymbolTable']['externalReferences']:
+            if c['name'] in dependencies:
+                if name in dependencies[c['name']]:
+                    continue
+                classes = dependencies[c['name']]
+            else:
+                classes = []
+            classes.append(name)
+            dependencies[c['name']] = classes
+
+    log.inf("===> Deleting metadata container")
+    r = tooling.delete('/sobjects/MetadataContainer/%s' % container['Id'])
+    return dependencies
---
+++ test_extractor.py
@@ -0,0 +1,66 @@
+import re
+from os import path, listdir
+from xml.etree import ElementTree as ET
+from zipfile import ZipFile
+
+XML_NAMESPACES = {'mt': 'http://soap.sforce.com/2006/04/metadata'}
+
+def grep_file(path, regobj):
+    try:
+        with open(path, 'r') as f:
+            if regobj.search(f.read()):
+                return True
+    except:
+        print("Could not process file %s" % path)
+    return False
+
+def extract_package_xml(zipfile):
+    with ZipFile(zipfile) as zf:
+        with zf.open('package.xml') as pxml:
+            return ET.fromstring(pxml.read().decode("utf-8"))
+
+def extract_members(root, object_type):
+    items = []
+    for node in root.findall("mt:types/[mt:name='%s']" % object_type, XML_NAMESPACES):
+        for member in node.findall("mt:members", XML_NAMESPACES):
+            items.append(member.text)
+    return items
+
+def extract_class_objects(zipfile, sourcedir):
+    root = extract_package_xml(zipfile)
+    return extract_members(root, 'ApexClass')
+
+def extract_test_objects(classes, sourcedir):
+    objects = []
+    regobj = re.compile(r'@isTest')
+    for c in classes:
+        filepath = path.join(sourcedir, "classes", "%s.cls" % c)
+        if not path.isfile(filepath):
+            continue
+        if grep_file(filepath, regobj):
+            objects.append(c)
+    
+    return objects
+
+def find_test_objects(sourcedir):
+    objects = []
+    regobj_test = re.compile(r'@isTest')
+    regobj_active = re.compile(r'Active')
+    classes_path = path.join(sourcedir, "classes")
+    for entry in listdir(classes_path):
+        if '-meta.xml' in entry:
+            cfname = entry.replace('-meta.xml', '')
+            cfpath = path.join(classes_path, cfname)
+            if path.isfile(cfpath):
+                if grep_file(cfpath, regobj_test) and grep_file(path.join(classes_path, entry), regobj_active):
+                    objects.append(cfname.replace(".cls", ""))
+    return objects
+
+def extract_class_names(objects, sourcedir):
+    class_names = []
+    regobj = re.compile(r'(private|public)\s+.*class\s+([a-zA-Z0-9_]+)\s*')
+    for o in objects:
+        with open(path.join(sourcedir, "classes", "%s.cls" % o), 'r') as f:
+            for m in regobj.finditer(f.read()):
+                class_names.append(m.group(2))
+    return class_names
